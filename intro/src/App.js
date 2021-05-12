f0b2590230b17134d97d5bca0f94f04eda38688f
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-05-12 12:51:09 -0700

    Created sample components

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Home from './components/Home'
+import About from './components/About'
+import Topics from './components/Topics'
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <About />
+      <Topics />
     </div>
   );
 }
---
+++ intro/src/components/Topics.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Topics() {
+    return <div>TOPICS</div>
+}
+
+export default Topics;

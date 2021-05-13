12d532e8bfc1b86db1eefb27b2701c64a19127ca
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-05-13 12:27:39 -0700

    Added react-router-dom w/ Router

--- intro/src/App.js
+++ intro/src/App.js
@@ -2,14 +2,25 @@ import './App.css';
 import Home from './components/Home'
 import About from './components/About'
 import Topics from './components/Topics'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <About />
-      <Topics />
-    </div>
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/topics'>Topics</Link></li>
+        </ul>
+
+        <hr />
+
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/topics' component={Topics} />
+      </div>
+    </BrowserRouter>
   );
 }
 

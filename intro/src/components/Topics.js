7f52a56dd21556b113cea12b7b3f705a7e24ea8f
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-05-13 13:41:57 -0700

    Example of dynamic urls

--- intro/src/components/Topics.js
+++ intro/src/components/Topics.js
@@ -5,20 +5,20 @@ function Topic({ match }) {
     return <h3>{match.params.topicId}</h3>
 }
 
-function Topics() {
+function Topics( { match } ) {
     return (
         <div>
             <h2>Topics</h2>
 
             <ul>
                 <li>
-                    <Link to='/topics/rendering'>Rendering with React</Link>
+                    <Link to={`${match.url}/rendering`}>Rendering with React</Link>
                 </li>
                 <li>
-                    <Link to='/topics/components'>Components</Link>
+                    <Link to={`${match.url}/components`}>Components</Link>
                 </li>
                 <li>
-                    <Link to='/topics/props-v-state'>Props vs State</Link>
+                    <Link to={`${match.url}/props-v-state`}>Props vs State</Link>
                 </li>
             </ul>
 

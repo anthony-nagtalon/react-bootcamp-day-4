686988380559dcfc4564d6c53545b7d212fe61c9
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-05-13 12:48:47 -0700

    Created nav list in Topics

--- intro/src/components/Topics.js
+++ intro/src/components/Topics.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { Link, Route } from 'react-router-dom'
+
+function Topic() {
+    return <h3>TOPIC</h3>
+}
 
 function Topics() {
-    return <div>TOPICS</div>
+    return (
+        <div>
+            <h2>Topics</h2>
+
+            <ul>
+                <li>
+                    <Link to=''>Rendering with React</Link>
+                </li>
+                <li>
+                    <Link to=''>Components</Link>
+                </li>
+                <li>
+                    <Link to=''>Props vs State</Link>
+                </li>
+            </ul>
+
+            <hr />
+
+            <Route path='/topics/rendering' component={Topic} />
+            <Route path='/topics/components' component={Topic} />
+            <Route path='/topics/props-v-state' component={Topic} />
+        </div>
+    )
 }
 
 export default Topics;
